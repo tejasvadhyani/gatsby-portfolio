8b15e5622cb796995f4c43fb61225adb265d5048
Tejasva Dhyani <tejasva.dhyani@paytm.com>
2019-04-17 12:45:35 +0530

    made changes to Layout page

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,9 +3,6 @@ import styles from './layout.module.css';
 
 export default ({ children }) => (
   <div className={styles.container}>
-    <div className={styles.header}>
-      Header
-    </div>
     {children}
   </div>
 );
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <div>
-    Work in progress...
+  <Layout>
+    <div>Work in progress...</div>
     <Link to="/">Go back to Homepage</Link>
-  </div>
+  </Layout>
 );
